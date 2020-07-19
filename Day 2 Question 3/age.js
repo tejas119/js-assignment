99a8e4746a07b181bc9b1909e2d373ab74dd4256
tejas119 <68263158+tejas119@users.noreply.github.com>
2020-07-19 09:52:00 +0530

    Add files via upload

---
+++ Day 2 Question 3/age.js
@@ -0,0 +1,12 @@
+{
+    var person = prompt("What is your age")
+    if (person > 21)
+    {
+        console.log("Can drink")
+
+    }
+    else
+    {
+        console.log("can't drink")
+    }
+    }
